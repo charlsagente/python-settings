71a4388a983f5cf88ef3223877427260dde98d9c
Felix Boehme <fsboehme@gmail.com>
2020-05-09 14:09:11 -0400

    raise AttributeError where it occurs; unit test

--- python_settings/__init__.py
+++ python_settings/__init__.py
@@ -107,7 +107,7 @@ class SetupSettings(object):
     def __getattr__(self, item):
         if self._wrapped is empty:
             self._setup(item)
-        get_attr = getattr(self._wrapped, item, empty)
+        get_attr = getattr(self._wrapped, item)
         if isinstance(get_attr, LazyProxy):
             try:
                 get_attr = get_attr()
@@ -118,8 +118,6 @@ class SetupSettings(object):
                     "LazySetting(MyCustomClass, [params])"
                     "Exception: %s - %s" % (type(ex), ex.__repr__())
                 )
-        if get_attr is empty:
-            raise AttributeError
         return get_attr
 
     def configure(self, default_settings, **options):
