199cf978f97cffec75a6fb7d63e6b15eb807c738
charlsagente <charlsagente@gmail.com>
2020-07-02 14:00:32 -0700

    setup for 0.2.2 and minor changes for unit tests

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='python-settings',
-    version='0.2.1',
+    version='0.2.2',
     packages=setuptools.find_packages(),
     url='https://github.com/charlsagente/python-settings',
     license='MIT',
